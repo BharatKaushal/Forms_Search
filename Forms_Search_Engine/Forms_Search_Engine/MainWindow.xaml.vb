00eb20281ef8133fa5c3f18507636a28f95c0fe2
Bharat Sharma <bharats@pbssystems.com>
2016-07-06 15:38:06 -0600

    Merge pull request #1 from BharatKaushal/first_commit
    07_06_2016

--- Forms_Search_Engine/Forms_Search_Engine/MainWindow.xaml.vb
+++ Forms_Search_Engine/Forms_Search_Engine/MainWindow.xaml.vb
@@ -1,13 +1,10 @@
 ﻿Imports System.Data.SqlClient
 Class MainWindow
-    Dim SQL As New SQLControl
+    Dim SQL As New DataManager
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        If SQL.HasConnection = True Then
-            SQL.RunQuery("SELECT * FROM FORMS_INFO WHERE ProgramName= '" & ProgramName.Text & "';")
-            If SQL.SQLDataSet.Tables.Count > 0 Then
-                dataGrid.ItemsSource = SQL.SQLDataSet.DefaultViewManager
-            End If
-        End If
+        'Dim l As New List(Of Mutant) From {New Mutant("From"), New Mutant("HELLO")}
+        'myDataGrid.ItemsSource = l
+        myDataGrid.ItemsSource = SQL.ExecuteQuery("SELECT * FROM FORMS_INFO WHERE ProgramName= '" & ProgramName.Text & "';").Item1
         'MessageBox.Show("Will Find " & ProgramName.Text & " with form ID " & FormID.Text & " when you have the logic in place")
     End Sub
 End Class
