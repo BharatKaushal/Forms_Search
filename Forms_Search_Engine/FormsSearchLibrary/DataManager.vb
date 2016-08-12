f4f808585c6cd6b08d1b950364c189a124b32fdf
Bharat Sharma <bharats@pbssystems.com>
2016-08-12 09:21:37 -0600

    Updating FileInfo in the Database when the file is modified after being added to the DB

--- Forms_Search_Engine/FormsSearchLibrary/DataManager.vb
+++ Forms_Search_Engine/FormsSearchLibrary/DataManager.vb
@@ -3,10 +3,29 @@ Imports System.Data.SqlClient
 Imports System.Data
 Public Class DataManager
     Implements IDataStore
+    Public Shared Function FormInfoChanged(query As String) As Date
+        Dim Result As String
+        Dim DateModi As Date
+        Dim connection As New SqlConnection("Server=MYPC\SQLEXPRESS;Database=FORMS_DATABASE;Trusted_Connection=Yes")
+        'Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
+        Try
+            connection.Open()
+            Dim GetDateModified As New SqlCommand(query, connection)
+            DateModi = Convert.ToString(GetDateModified.ExecuteScalar)
+            Return DateModi
+        Catch ex As Exception
+            Result = ex.Message & vbNewLine
+            Result &= ex.StackTrace.ToString
+            Return DateModi
+        Finally
+            connection.Close()
+        End Try
+    End Function
     Public Shared Function CheckDatabase(query As String) As Boolean
         Dim Result As String = String.Empty
         Dim AlreadyExist As Boolean
-        Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
+        Dim connection As New SqlConnection("Server=MYPC\SQLEXPRESS;Database=FORMS_DATABASE;Trusted_Connection=Yes")
+        'Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
         Try
             connection.Open()
             Dim CheckUserName As New SqlCommand(query, connection)
@@ -23,7 +42,8 @@ Public Class DataManager
     Public Shared Function UpdateDatabase(query As String) As Boolean
         Dim data As SqlDataReader = Nothing
         Dim Result As String = String.Empty
-        Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
+        Dim connection As New SqlConnection("Server=MYPC\SQLEXPRESS;Database=FORMS_DATABASE;Trusted_Connection=Yes")
+        'Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
         Try
             connection.Open()
             Dim command As New SqlCommand(query, connection)
@@ -41,7 +61,8 @@ Public Class DataManager
         Dim data As SqlDataReader = Nothing
         Dim Result As String = "All good"
         Try
-            Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
+            Dim connection As New SqlConnection("Server=MYPC\SQLEXPRESS;Database=FORMS_DATABASE;Trusted_Connection=Yes")
+            'Dim connection As New SqlConnection("Server=BHARATS03D\SQLEXPRESS;Database=FORMS_DATABASE;User=pbsuser;Pwd=pbs8805") 'Trusted_Connection=Yes")
             connection.Open()
             Dim command As New SqlCommand(query, connection)
             data = command.ExecuteReader()
